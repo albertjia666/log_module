2e27aa2e3411f8d27276d2a04ee2065786e5f74a
albertjia666 <32834700+albertjia666@users.noreply.github.com>
2021-07-09 11:32:57 +0800

    Update logger_template_c.py

--- logger_template_c.py
+++ logger_template_c.py
@@ -1,14 +1,46 @@
 # -*- coding: utf-8 -*-
 import logging
+import os
+import sys
 from pythonjsonlogger import jsonlogger
 
-logger = logging.getLogger(name="test")
+class Log(object):
 
-ch = logging.StreamHandler()
-fmt = jsonlogger.JsonFormatter('%(asctime)s %(name)s %(levelname)s %(filename)s %(funcName)s:%(lineno)d %(message)s')
-ch.setFormatter(fmt)
-logger.setLevel(logging.DEBUG)
-logger.addHandler(ch)
-logger.info("PROGRESS", extra={"event_type": "EventType.PROGRESS",
-                               "status": "运行中",
-                               "total": 100, "current": 5})
+    def __init__(self, file_name=None, log_name=None):
+
+        self.logger = logging.getLogger(name=file_name)
+        self.logger.setLevel(level=logging.INFO)
+        self.console_format = jsonlogger.JsonFormatter('%(asctime)s %(name)s %(filename)s %(lineno)d %(levelname)s %(message)s')
+        self.file_format = jsonlogger.JsonFormatter('%(asctime)s %(name)s %(filename)s %(lineno)d %(levelname)s %(message)s')
+        self.log_dir = os.path.join(sys.path[0], 'logs', log_name)
+
+        if not self.logger.handlers:
+            # add a console handler
+            ch = logging.StreamHandler()
+            ch.setLevel(level=logging.INFO)
+            ch.setFormatter(self.console_format)
+            self.logger.addHandler(ch)
+
+            # add a file handler
+            fh = logging.FileHandler(filename=self.log_dir, encoding='utf-8')
+            fh.setLevel(level=logging.INFO)
+            fh.setFormatter(self.file_format)
+            self.logger.addHandler(fh)
+
+    def init_logger(self):
+        return self.logger
+
+
+if __name__ == '__main__':
+
+    logger = Log(file_name=None, log_name='test.log').init_logger()
+
+    logger.info('======[INFO TEST]======')
+    logger.debug('======[DEBUG TEST]======')
+    logger.warning('======[WARNING TEST]======')
+    logger.error('======[ERROR TEST]======')
+    logger.critical('======[CRITICAL TEST]======')
+
+    # add extra key-value logs
+    logger.info("PROGRESS", extra={"key": "value"})
+

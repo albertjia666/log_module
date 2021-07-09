c0530987ee752c29e30d6cc9b69a9a7a9e22cc16
albertjia666 <32834700+albertjia666@users.noreply.github.com>
2021-07-09 13:14:47 +0800

    Update logger_template_a.py

--- logger_template_a.py
+++ logger_template_a.py
@@ -10,8 +10,8 @@ class MyLogger(object):
 
     def __init__(self, logger_c, logger_f, log_name):
 
-        self.logger_c = logging.getLogger(name=logger_c) # 定义对应的程序模块名name，默认是root
-        self.logger_c.setLevel(level=logging.DEBUG) # 指定最低的日志级别 critical > error > warning > info > debug
+        self.logger_c = logging.getLogger(name=logger_c)  # 定义对应的程序模块名name，默认是root
+        self.logger_c.setLevel(level=logging.DEBUG)  # 指定最低的日志级别 critical > error > warning > info > debug
 
         self.logger_f = logging.getLogger(name=logger_f)
         self.logger_f.setLevel(level=logging.DEBUG)
@@ -20,50 +20,48 @@ class MyLogger(object):
         log_name = os.getcwd() + "/logs/" + log_name + ".log"
 
         # 定义handler的输出格式
-        formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(name)s - %(message)s")
-        
+        # formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(name)s - %(message)s")
+        formatter = logging.Formatter("%(asctime)s - %(name)s [line:%(lineno)d] - %(message)s")
+
         # 这里进行判断，如果logger.handlers列表为空，则添加，否则，直接去写日志，解决重复打印的问题
         if not self.logger_c.handlers:
-
-            self.ch = logging.StreamHandler(stream=sys.stdout) # 日志输出到屏幕控制台
-            self.ch.setLevel(level=logging.DEBUG) # 设置日志等级
+            self.ch = logging.StreamHandler()  # 日志输出到屏幕控制台
+            self.ch.setLevel(level=logging.DEBUG)  # 设置日志等级
             self.ch.setFormatter(fmt=formatter)
-            self.logger_c.addHandler(self.ch) # 给logger添加handler
+            self.logger_c.addHandler(self.ch)  # 给logger添加handler
 
         if not self.logger_f.handlers:
-
-            self.fh = logging.FileHandler(filename=log_name, encoding='utf-8') # 日志写入文件
+            self.fh = logging.FileHandler(filename=log_name, encoding='utf-8')  # 日志写入文件
             self.fh.setLevel(level=logging.DEBUG)
             self.fh.setFormatter(fmt=formatter)
             self.logger_f.addHandler(self.fh)
 
     def debug(self, message):
-        self.logger_c.debug(Fore.BLUE + "DEBUG - " + str(message) + Style.RESET_ALL)
+        self.logger_c.debug(Fore.BLUE + "[DEBUG] - " + str(message) + Style.RESET_ALL)
         self.logger_f.debug(str(message))
 
     def info(self, message):
-        self.logger_c.info(Fore.GREEN + "INFO - " + str(message) + Style.RESET_ALL)
+        self.logger_c.info(Fore.GREEN + "[INFO] - " + str(message) + Style.RESET_ALL)
         self.logger_f.info(str(message))
 
     def warning(self, message):
-        self.logger_c.warning(Fore.YELLOW + "WARNING - " + str(message) + Style.RESET_ALL)
+        self.logger_c.warning(Fore.YELLOW + "[WARNING] - " + str(message) + Style.RESET_ALL)
         self.logger_f.warning(str(message))
 
     def error(self, message):
-        self.logger_c.error(Fore.LIGHTRED_EX + "ERROR - " + str(message) + Style.RESET_ALL)
+        self.logger_c.error(Fore.CYAN + "[ERROR] - " + str(message) + Style.RESET_ALL)
         self.logger_f.error(str(message))
 
     def critical(self, message):
-        self.logger_c.critical(Fore.RED + "CRITICAL - " + str(message) + Style.RESET_ALL)
+        self.logger_c.critical(Fore.RED + "[CRITICAL] - " + str(message) + Style.RESET_ALL)
         self.logger_f.critical(str(message))
 
 
 if __name__ == '__main__':
 
-    log = MyLogger(logger_c="Console", logger_f="FileLog", log_name="test")
+    log = MyLogger(logger_c="[console] [x.py]", logger_f="[file] [x.py]", log_name="test")
     log.debug("this is a test")
     log.info("this is a test")
     log.warning("this is a test")
     log.error("this is a test")
     log.critical("this is a test")
-    
